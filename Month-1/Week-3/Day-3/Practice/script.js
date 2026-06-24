aa085e38b8329dd81101cb8fa3132916dbae98e5
rekha@116#sradha <rekhamandal1125@gmail.com>
2026-06-24 18:52:49 +0530

    comparision,logical,conditional statement

---
+++ Month-1/Week-3/Day-3/Practice/script.js
@@ -0,0 +1,42 @@
+console.log("hello welcome to third class");
+
+// comparision operator
+// >,<,>=,<=,==,===,/=,/==
+
+console.log(3>5);
+console.log(6<10);
+console.log(3>=3);
+
+console.log(2=2);
+console.log(5 == 6);
+console.log("hii" == "hii");
+console.log("hii" == 2);
+console.log("hii" == 2);
+console.log("2" == 2);
+
+// == (strict checking)
+console.log(+"2" === 2) // true
+
+console.log({} == {})  //false
+console.log({} === {})  //
+
+let obj1={
+
+}
+let obj2=obj1;
+
+console.log(obj1 == obj2);
+
+console.log(2 != true+1);
+console.log(1 !== "1");
+
+// logical operator
+// &&,||,!
+
+console.log((2<1) && true);
+
+console.log((4 ==="4") || (2<=2));
+
+console.log(!true) // false
+console.log(!false) // true
+
