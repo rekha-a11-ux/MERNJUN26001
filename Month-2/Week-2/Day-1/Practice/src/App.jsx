170d6fce34f2202324644ae44532cd8d41cbd8ae
rekha@116#sradha <rekhamandal1125@gmail.com>
2026-07-07 12:40:39 +0530

    react,hrllo

---
+++ Month-2/Week-2/Day-1/Practice/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>hello</h1>
+    <Navbar />
+    
+    </>
+    
+
+  )
+}
+
+export default App
